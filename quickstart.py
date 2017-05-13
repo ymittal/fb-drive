c3a6d6e727fa05339f78c994bf4bf19df3dee932
Yash Mittal <yashmittal2009@gmail.com>
2017-05-13 17:03:31 -0400

    Cleaning up

--- quickstart.py
+++ quickstart.py
@@ -13,14 +13,6 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/fb-drive.json
 SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 FB_CONFIG_FILE = 'fb.json'
