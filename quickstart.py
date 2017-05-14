dec1d69db02153e8c01799762447d5a7bbafe9e7
Yash Mittal <yashmittal2009@gmail.com>
2017-05-13 20:31:43 -0400

    Basic multithreading implemented (one batch only right now)

--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from threading import Thread
 import httplib2
 import json
 import os
@@ -8,16 +9,20 @@ from fbrecog import recognize
 
 from apiclient import discovery
 from apiclient import errors
+from apiclient.http import HttpRequest
 from apiclient.http import MediaIoBaseDownload
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
+APPLICATION_NAME = 'Drive and FB Image'
 SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+
 CLIENT_SECRET_FILE = 'client_secret.json'
 FB_CONFIG_FILE = 'fb.json'
-APPLICATION_NAME = 'Drive and FB Image'
 DATA_FILE = 'data.json'
+
+MAX_THREADS = 16
 
 with open(FB_CONFIG_FILE) as fb_config_file:
     config = json.load(fb_config_file)
@@ -60,14 +65,22 @@ def get_credentials():
     return credentials
 
 
-def download_file(drive_service, file_id, filename):
+def download_file(file_id, filename):
+    drive_service = discovery.build('drive', 'v3', http=getHttp())
     request = drive_service.files().get_media(fileId=file_id)
+
     fh = io.FileIO(filename, 'wb')
     downloader = MediaIoBaseDownload(fh, request)
+
     done = False
     while done is False:
-        status, done = downloader.next_chunk()
+        try:
+            status, done = downloader.next_chunk()
+        except Exception as ex:
+            print ("User rate limit exceeded for %s" % filename)
+            return False
         print ("Download %d%%." % int(status.progress() * 100))
+    return True
 
 
 def recognize_pic(pic_name):
@@ -76,28 +89,37 @@ def recognize_pic(pic_name):
     return result
 
 
-def classify_pic(service, pic_id, pic_name):
+def classify_pic(pic_id, pic_name):
     if pic_id not in classify_data:
-        download_file(service, pic_id, pic_name)
-        classify_data[pic_id] = recognize_pic(pic_name)
+        is_success = download_file(pic_id, pic_name)
+        if is_success:
+            classify_data[pic_id] = recognize_pic(pic_name)
+        # delete picture from current dir
     else:
         print ('%s already classified' % pic_name)
 
 
-def classify_pics(service, picTuples):
-    """
-    """
+def classify_pics(picTuples):
     try:
-        for picTuple in picTuples:
-            classify_pic(service, picTuple[0], picTuple[1])
+        print ("Starting threads...")
+        threads = []
+        for picTuple in picTuples[:MAX_THREADS]:
+            t = Thread(target=classify_pic,
+                       args=(picTuple[0], picTuple[1]))
+            threads.append(t)
+            t.start()
+
+        for t in threads:
+            t.join()
+
     finally:
         with open(DATA_FILE, 'w') as data_file:
+            print ("Rewriting %s with %s entries" %
+                   (DATA_FILE, len(classify_data)))
             json.dump(classify_data, data_file)
 
 
 def retrieve_pics(drive_service):
-    """
-    """
     picTuples = []
     page_token = None
     while True:
@@ -109,7 +131,6 @@ def retrieve_pics(drive_service):
         for file in response.get('files', []):
             if file.get('id') not in classify_data:
                 picTuples.append((file.get('id'), file.get('name')))
-                # print ('Found file: %s (%s)' % (file.get('name'), file.get('id')))
 
         page_token = response.get('nextPageToken', None)
         if page_token is None:
@@ -119,18 +140,22 @@ def retrieve_pics(drive_service):
     return picTuples
 
 
+def getHttp():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    return http
+
+
 def main():
     """Shows basic usage of the Google Drive API.
 
     Creates a Google Drive API service object and outputs the names and IDs
     for up to 10 files.
     """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('drive', 'v3', http=http)
+    service = discovery.build('drive', 'v3', http=getHttp())
 
     picTuples = retrieve_pics(service)
-    classify_pics(service, picTuples)
+    classify_pics(picTuples)
 
 
 if __name__ == '__main__':
